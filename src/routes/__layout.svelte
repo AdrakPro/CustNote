618df7281d18fc6ef1775553d16752f7366bebcc
AdrakPro <adrak99.adrak99@gmail.com>
2022-06-29 01:17:52 +0200

    First version of welcome page (without responsive nav)

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,10 +3,125 @@
 </script>
 
 <header>
-    <div></div>
+    <div class="logo"><img src="favicon.png" width="64" height="64" alt="icon"></div>
+    <div class="row">
+        <nav class="top-nav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Why CustNote?</a></li>
+                <li><a href="#">Plans</a></li>
+            </ul>
+        </nav>
+        <nav class="utility-nav">
+            <ul>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
-<main>
-    <slot/>
-</main>
-<footer>
-</footer>
+
+<section>
+    <div class="header-dummy"></div>
+    <slot />
+</section>
+
+<footer></footer>
+
+<style lang="scss">
+    /* HEADER */
+    header {
+      position: fixed;
+      display: flex;
+      width: 100%;
+      height: 64px;
+      z-index: 999;
+    }
+
+    /* NAVIGATION */
+    nav {
+      ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+      }
+
+      a {
+        transition: opacity 150ms ease-in-out;
+        color: #e7e4f9;
+        text-decoration: none;
+        font-size: 1.2rem;
+        opacity: 0;
+
+        &:hover {
+          opacity: .9;
+        }
+      }
+    }
+
+    /* RESPONSIVE STYLE WHEN SCREEN EXCEEDS 900PX */
+    @media screen and (min-width: 900px) {
+        nav {
+          /* Edge support */
+          position: relative;
+          top: initial;
+          left: initial;
+          transform: scale(1, 1);
+          transition: none;
+          text-align: left;
+          background: none;
+          /* end Edge support */
+
+          display: flex;
+          align-items: center;
+
+          ul {
+            display: flex;
+          }
+
+          li {
+            line-height: 36px;
+            margin-left: 2em;
+          }
+
+          a {
+            position: relative;
+            opacity: 1;
+
+            &::before {
+              display: block;
+              position: absolute;
+              height: 3px;
+              content: '';
+              top: 1.5em;
+              left: 0;
+              right: 0;
+              transform: scale(0, 1);
+              transition: transform ease-in-out 250ms;
+              background: #46BDDF;
+            }
+
+            &:hover::before {
+              transform: scale(1, 1);
+            }
+          }
+        }
+    }
+
+    /* Experimental - used to fix fixed header */
+    .header-dummy {
+      height: 64px;
+    }
+
+    .row {
+      width: 100%;
+      display: flex;
+    }
+
+    /* Experimental - used to align li elements to the right of the nav */
+    .utility-nav {
+      margin-left: auto;
+      margin-right: 20px;
+    }
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,61 @@
 <script>
 
 </script>
-<div class="wave">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path fill="#46BDDF" fill-opacity="0.95" d="M0,64L48,90.7C96,117,192,171,288,160C384,149,480,75,576,64C672,53,768,107,864,117.3C960,128,1056,96,1152,101.3C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-    </svg>
-    <div class="wave-extension"></div>
+
+<div class="welcome-page">
+    <div class="info">
+        <h1 class="quote">A better way to write, organize and share your notes</h1>
+        <p>Take your notes to the next level with a blazing fast speed and various customizing features</p>
+        <button>Get started now</button>
+        <button>See how CustNote works</button>
+    </div>
+    <div class="wave">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#46BDDF" fill-opacity="0.95" d="M0,64L48,90.7C96,117,192,171,288,160C384,149,480,75,576,64C672,53,768,107,864,117.3C960,128,1056,96,1152,101.3C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+        </svg>
+        <div class="wave-extension"></div>
+    </div>
 </div>
 
 <style lang="scss">
-  /* Wave */
+  .welcome-page {
+    position: relative;
+    height: calc(89vh - 1px);
+  }
 
+  .info {
+    text-align: center;
+    margin-top: 2rem;
+  }
+
+  .quote {
+    font-weight: 600;
+    font-size: 44px;
+    letter-spacing: -.008em;
+    margin-bottom: 20px;
+  }
+
+  p {
+    font-size: 18px;
+    margin-bottom: 24px;
+  }
+
+  /* WAVE */
   .wave {
-    position: fixed;
-    width: 100vw;
+    position: absolute;
+    width: 100%;
     bottom: 0;
     color: #46BDDF;
 
     svg {
       display: block;
+      transform: translateY(1px);
     }
   }
 
   .wave-extension {
     width: 100%;
-    height: 10vh;
+    height: 12vh;
     background-color: #46BDDF;
     opacity: 0.95;
   }
