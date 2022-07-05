e16fd4ecb2f412f3695a1551cac72c3336844932
AdrakPro <adrak99.adrak99@gmail.com>
2022-07-05 22:40:24 +0200

    Welcome page designed

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,127 +1,213 @@
 <script>
     import '../styles/global.css';
+
+    let isOpenMenu = false;
 </script>
 
-<header>
-    <div class="logo"><img src="favicon.png" width="64" height="64" alt="icon"></div>
-    <div class="row">
-        <nav class="top-nav">
+<div class:mobile-nav={ isOpenMenu }>
+    <header>
+        <div class="logo">
+            <img src="favicon.png" width="64" height="64" alt="icon">
+        </div>
+        <input type="checkbox" id="toggle-menu" class="toggle-menu" bind:checked={ isOpenMenu }>
+        <nav>
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Why CustNote?</a></li>
                 <li><a href="#">Plans</a></li>
             </ul>
-        </nav>
-        <nav class="utility-nav">
-            <ul>
+            <ul class:utility-nav={ !isOpenMenu }>
                 <li><a href="#">Help</a></li>
                 <li><a href="#">Login</a></li>
             </ul>
         </nav>
-    </div>
-</header>
+        <label for="toggle-menu" class="toggle-menu-label">
+            <span></span>
+        </label>
+    </header>
 
-<section>
-    <div class="header-dummy"></div>
-    <slot />
-</section>
+    <main>
+        <div class="header-dummy"></div>
+        <slot />
+    </main>
 
-<footer></footer>
+    <footer></footer>
+</div>
 
 <style lang="scss">
-    /* HEADER */
-    header {
-      position: fixed;
-      display: flex;
-      width: 100%;
-      height: 64px;
-      z-index: 999;
+  /* FULL PAGE OVERLAY */
+  .mobile-nav {
+    main, .logo {
+      filter: brightness(45%);
+    }
+  }
+
+  /* HEADER */
+  header {
+    position: fixed;
+    width: 100%;
+    height: 64px;
+    z-index: 999;
+  }
+
+  /* NAVIGATION */
+  nav {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    transform: scale(1, 0);
+    transform-origin: top;
+    transition: transform 400ms ease-in-out;
+    padding-top: 1em;
+
+    ul {
+      margin: 0;
+      padding: 0;
+      list-style: none;
     }
 
-    /* NAVIGATION */
+    li {
+      text-align: center;
+      margin-bottom: 1.6em;
+    }
+
+    a {
+      transition: opacity 150ms ease-in-out;
+      color: #e7e4f9;
+      text-decoration: none;
+      font-size: 1.2em;
+      opacity: 0;
+      text-align: center;
+    }
+  }
+
+  /* TOGGLE MENU BUTTON */
+  /* Allow menu button to me taggable */
+  .toggle-menu {
+    position: absolute !important;
+    top: -9999px !important;
+    left: -9999px !important;
+  }
+
+  /* Todo - Add circle instead of outline */
+  .toggle-menu:focus ~ .toggle-menu-label {
+    margin-top: 1px;
+    outline: 1px solid white;
+  }
+
+  .toggle-menu:checked ~ nav {
+    transform: scale(1, 1);
+  }
+
+  .toggle-menu:checked ~ nav a {
+    opacity: 1;
+    transition: opacity 250ms ease-in-out 250ms;
+  }
+
+  .toggle-menu-label {
+    display: flex;
+    position: absolute;
+    height: 100%;
+    top: 0;
+    right: 0;
+    margin-right: 1em;
+    align-items: center;
+  }
+
+  .toggle-menu-label span,
+  .toggle-menu-label span::before,
+  .toggle-menu-label span::after {
+    display: block;
+    position: relative;
+    height: 2px;
+    width: 1.7rem;
+    background: #e7e4f9;
+    border-radius: 2px;
+  }
+
+  .toggle-menu-label span::before,
+  .toggle-menu-label span::after {
+    position: absolute;
+    content: "";
+  }
+
+  .toggle-menu-label span::before {
+    bottom: 7px;
+  }
+
+  .toggle-menu-label span::after {
+    top: 7px;
+  }
+
+  /* TODO CLOSE MENU BUTTON */
+  /* RESPONSIVE STYLE WHEN SCREEN EXCEEDS 900PX */
+  @media screen and (min-width: 900px) {
+    .toggle-menu-label {
+      display: none;
+    }
+
+    header {
+      display: flex;
+    }
+
     nav {
+      display: flex;
+
+      /* Edge support */
+      position: relative;
+      top: initial;
+      left: initial;
+      transform: scale(1, 1);
+      transition: none;
+      text-align: left;
+      background: none;
+
       ul {
-        margin: 0;
-        padding: 0;
-        list-style: none;
+        display: flex;
       }
 
+      li {
+        line-height: 36px;
+        margin-left: 2em;
+      }
+
+      /* Todo - Based on site hover a nav button (Welcome page -> Home button is hovered) */
       a {
-        transition: opacity 150ms ease-in-out;
-        color: #e7e4f9;
-        text-decoration: none;
-        font-size: 1.2rem;
-        opacity: 0;
+        position: relative;
+        opacity: 1;
+
+        &::before {
+          display: block;
+          position: absolute;
+          height: 3px;
+          top: 1.5em;
+          left: 0;
+          right: 0;
+          transform: scale(0, 1);
+          transition: transform ease-in-out 250ms;
+          content: "";
+          background: #46bddf;
+        }
 
         &:hover {
-          opacity: .9;
+          opacity: 0.9;
+        }
+
+        &:hover::before {
+          transform: scale(1, 1);
         }
       }
     }
+  }
 
-    /* RESPONSIVE STYLE WHEN SCREEN EXCEEDS 900PX */
-    @media screen and (min-width: 900px) {
-        nav {
-          /* Edge support */
-          position: relative;
-          top: initial;
-          left: initial;
-          transform: scale(1, 1);
-          transition: none;
-          text-align: left;
-          background: none;
-          /* end Edge support */
+  /* Experimental - used to fix fixed header */
+  .header-dummy {
+    height: 64px;
+  }
 
-          display: flex;
-          align-items: center;
-
-          ul {
-            display: flex;
-          }
-
-          li {
-            line-height: 36px;
-            margin-left: 2em;
-          }
-
-          a {
-            position: relative;
-            opacity: 1;
-
-            &::before {
-              display: block;
-              position: absolute;
-              height: 3px;
-              content: '';
-              top: 1.5em;
-              left: 0;
-              right: 0;
-              transform: scale(0, 1);
-              transition: transform ease-in-out 250ms;
-              background: #46BDDF;
-            }
-
-            &:hover::before {
-              transform: scale(1, 1);
-            }
-          }
-        }
-    }
-
-    /* Experimental - used to fix fixed header */
-    .header-dummy {
-      height: 64px;
-    }
-
-    .row {
-      width: 100%;
-      display: flex;
-    }
-
-    /* Experimental - used to align li elements to the right of the nav */
-    .utility-nav {
-      margin-left: auto;
-      margin-right: 20px;
-    }
-
+  /* Experimental - used to align li elements to the right of the nav */
+  .utility-nav {
+    margin-left: auto;
+    margin-right: 1.5em;
+  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,8 @@
 
 </script>
 
-<div class="welcome-page">
-    <div class="info">
+<div class="info">
+    <div class="info-text">
         <h1 class="quote">A better way to write, organize and share your notes</h1>
         <p>Take your notes to the next level with a blazing fast speed and various customizing features</p>
         <button>Get started now</button>
@@ -11,33 +11,50 @@
     </div>
     <div class="wave">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#46BDDF" fill-opacity="0.95" d="M0,64L48,90.7C96,117,192,171,288,160C384,149,480,75,576,64C672,53,768,107,864,117.3C960,128,1056,96,1152,101.3C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+            <path d="M0,64L48,90.7C96,117,192,171,288,160C384,149,480,75,576,64C672,53,768,107,864,117.3C960,128,1056,96,1152,101.3C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>
         <div class="wave-extension"></div>
     </div>
 </div>
 
 <style lang="scss">
-  .welcome-page {
+  /* INFO PAGE*/
+  .info {
     position: relative;
     height: calc(89vh - 1px);
   }
 
-  .info {
+  .info-text {
     text-align: center;
-    margin-top: 2rem;
+    margin-top: 2em;
+    padding: 0 5px 0 5px;
   }
 
   .quote {
     font-weight: 600;
     font-size: 44px;
     letter-spacing: -.008em;
-    margin-bottom: 20px;
+    margin-bottom: .7em;
   }
 
   p {
     font-size: 18px;
-    margin-bottom: 24px;
+    margin-bottom: 1em;
+  }
+
+  /* BUTTONS TODO STYLING */
+  button {
+    display: inline-block;
+    position: relative;
+    height: 48px;
+    margin: 0 10px;
+    line-height: 48px;
+    text-align: left;
+    border-radius: 5px;
+    border: 1px solid transparent;
+    color: #e7e4f9;
+    background-color: #46BDDF;
+    font-size: inherit;
   }
 
   /* WAVE */
@@ -50,6 +67,8 @@
     svg {
       display: block;
       transform: translateY(1px);
+      fill: #46BDDF;
+      fill-opacity: .95;
     }
   }
 
@@ -57,6 +76,6 @@
     width: 100%;
     height: 12vh;
     background-color: #46BDDF;
-    opacity: 0.95;
+    opacity: .95;
   }
 </style>
