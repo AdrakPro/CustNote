f1701eaacca11d4b554c82953c7064b90f9e9b1d
AdrakPro <adrak99.adrak99@gmail.com>
2022-07-27 19:04:00 +0200

    Removed airbnb style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: [
-		'airbnb-typescript',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:promise/recommended',
 		'prettier',
