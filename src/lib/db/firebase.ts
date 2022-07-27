b8728857fd673e420317ff51b6a3abfcfc13a0ba
AdrakPro <adrak99.adrak99@gmail.com>
2022-07-27 20:12:01 +0200

    Cleanup & add shared scss

--- src/lib/db/firebase.ts
+++ src/lib/db/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth  } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { doc, getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,13 +9,13 @@ import { doc, getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAaIIJhfRy6Mhik1OkYbhgzNdk3l9Jn1TY',
-  authDomain: 'custnote-38c35.firebaseapp.com',
-  projectId: 'custnote-38c35',
-  storageBucket: 'custnote-38c35.appspot.com',
-  messagingSenderId: '554174210361',
-  appId: '1:554174210361:web:a4118343fda470722ec8e8',
-  measurementId: 'G-RL5TBB5YHE'
+	apiKey: 'AIzaSyAaIIJhfRy6Mhik1OkYbhgzNdk3l9Jn1TY',
+	authDomain: 'custnote-38c35.firebaseapp.com',
+	projectId: 'custnote-38c35',
+	storageBucket: 'custnote-38c35.appspot.com',
+	messagingSenderId: '554174210361',
+	appId: '1:554174210361:web:a4118343fda470722ec8e8',
+	measurementId: 'G-RL5TBB5YHE',
 };
 
 // Initialize Firebase
@@ -23,11 +23,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const userDoc = (userId : string) => doc(db, 'users', userId);
+const userDoc = (userId: string) => doc(db, 'users', userId);
 
-
-export {
-  auth,
-  db,
-  userDoc,
-};
+export { auth, db, userDoc };
