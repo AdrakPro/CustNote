c53fdb75fd7b71c01085d83e1192fddac30e76b5
AdrakPro <adrak99.adrak99@gmail.com>
2022-08-04 21:05:00 +0200

    Refactoring

---
+++ src/lib/components/auth/SignOut.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { auth } from '$lib/db/firebase';
+	import { signOut } from 'firebase/auth';
+
+	function signOutUser() {
+		signOut(auth).then(() => {
+			localStorage.removeItem('uid');
+		}).catch((e) => console.log(e));
+	}
+</script>
+
+<li><a href="/" on:click={ () => signOutUser() }>Log out</a></li>
--- src/lib/components/navbar/NotesNav.svelte
+++ src/lib/components/navbar/NotesNav.svelte
@@ -1,17 +1,10 @@
-<script lang="ts">
-	import { auth } from '$lib/db/firebase';
-	import { signOut } from 'firebase/auth';
-
-	function logOut() {
-		signOut(auth).then(() => {
-			localStorage.removeItem('uid');
-		}).catch((e) => console.log(e));
-	}
+<script>
+	import SignOut from '$lib/components/auth/SignOut.svelte';
 </script>
 
 <nav>
   <ul>
     <li><a href="/">Home</a></li>
-    <li><a href="/" on:click={ () => logOut() }>Log out</a></li>
+    <SignOut />
   </ul>
 </nav>
