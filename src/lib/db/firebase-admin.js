770b6470a98b945a496d95e32b69df07aa936432
AdrakPro <adrak99.adrak99@gmail.com>
2022-08-15 19:56:20 +0200

    Added encrypted service account

--- src/lib/db/firebase-admin.js
+++ src/lib/db/firebase-admin.js
@@ -1,8 +1,10 @@
-import * as credentials from './admin-key.json';
 import admin from 'firebase-admin';
+import { SERVICE_ACCOUNT } from '$lib/utils/constans.js';
+
+const serviceAccount = JSON.parse(SERVICE_ACCOUNT);
 
 admin.initializeApp({
-	credential: admin.credential.cert(credentials),
+	credential: admin.credential.cert(serviceAccount),
 });
 
 export const auth = admin.auth;
