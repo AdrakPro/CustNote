6d9042a9fa83088fc91e16418ede50f2b8b4a84c
AdrakPro <adrak99.adrak99@gmail.com>
2022-08-27 17:21:36 +0200

    Refactor Icon

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	export let width = 32;
 	export let height = 32;
-	export let style = '';
 	export let src: string;
 	export let onClick = () => {
 	};
 
 </script>
 
-<img { height } { src } { style } { width } on:click={ onClick } alt="icon" />
+<img { height } { src } { width } style="{ $$props.style }" on:click={ onClick } alt="icon" />
