8a63c3356b3bfcdc40ed9281ac97cfbdf786c145
AdrakPro <adrak99.adrak99@gmail.com>
2022-08-27 17:19:09 +0200

    Restyling error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,32 +1,29 @@
 <script>
 	import { page } from '$app/stores';
 	// svelte-kit bug redirect causes error page
-	console.log($page.error, $page.error.message);
 </script>
 
 <div class="container">
-	<h1 class="title">Oops something went wrong with status code: { $page.error.status }</h1>
-	<p class="home"><a href="/">Go back home</a></p>
-	<img alt="gif" src="https://c.tenor.com/7tzqV8r23qoAAAAd/jd.gif" />
+	<h1 class="error">{ $page.status } : { $page.error.message }</h1>
+	<img alt="dancing monster" src="https://c.tenor.com/7tzqV8r23qoAAAAd/jd.gif" />
 </div>
 
 <style lang="scss">
   .container {
-    text-align: center;
-  }
+		height: 100vh;
+		display: grid;
+		gap: $s-32;
+		place-content: center;
+		place-items: center;
 
-  .title {
-    font-size: $fs-32;
-    font-weight: $fw-semibold;
-    margin: $s-16 0 $s-16;
-  }
-
-  .home {
-    font-size: $fs-24;
-    margin-bottom: $s-16;
-
-    a {
-      color: $blue;
+    .error {
+      font-size: $fs-42;
+      font-weight: $fw-semibold;
     }
+
+    img {
+			border-radius: $base-radius;
+			object-fit: cover;
+		}
   }
 </style>
