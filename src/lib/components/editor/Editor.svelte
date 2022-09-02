1add80f3de9e57664203e5242075ea53acbab3ce
AdrakPro <adrak99.adrak99@gmail.com>
2022-09-02 22:15:50 +0200

    Boilerplate editor

---
+++ src/lib/components/editor/Editor.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
+	import { commonmark } from '@milkdown/preset-commonmark';
+	import { nord } from '@milkdown/theme-nord';
+
+	export let defaultValue: string;
+
+	function editor(dom) {
+		Editor.make()
+			.config((ctx) => {
+				ctx.set(rootCtx, dom);
+				ctx.set(defaultValueCtx, defaultValue);
+			})
+			.use(nord)
+			.use(commonmark)
+			.create();
+	}
+</script>
+
+<style></style>
+
+<div use:editor></div>
