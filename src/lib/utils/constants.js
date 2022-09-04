e0edecbf460b2908c4c3ca4f3fc80881092bf1f0
AdrakPro <adrak99.adrak99@gmail.com>
2022-09-04 21:13:00 +0200

    Added persistent store

--- src/lib/utils/constants.js
+++ src/lib/utils/constants.js
@@ -1,8 +1,13 @@
+// refactor meta
 export const WEB_API_KEY = import.meta.env.VITE_WEB_API_KEY;
 export const SECURE = import.meta.env.VITE_SECURE;
 export const SERVICE_ACCOUNT = import.meta.env
 	.VITE_FIREBASE_SERVICE_ACCOUNT_KEY;
+export const ENCRYPTION_KEY = import.meta.env.VITE_ENCRYPTION_KEY;
 export const FIREBASE_CONFIG = import.meta.env.VITE_FIREBASE_CONFIG;
-export const DEV = import.meta.env.VERCEL_ENV;
+export const DEV = import.meta.env.DEV;
+
 export const USER = 'user';
 export const MODULE = 'module';
+export const MODULES = 'modules';
+export const NOTES = 'notes';
